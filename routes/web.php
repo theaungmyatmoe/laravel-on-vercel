bca1e2a4522c4738012e06cd43477c92d3898991
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-10 22:56:25 +0630

    feat: add ping pong in web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/ping', fn () => response()->json([
+    'msg' => 'pong'
+]));
