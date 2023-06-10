7779320b88169b55b3980e51018d5401b8e2032f
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-10 23:04:22 +0630

    remove prefix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->prefix('api')
                 ->group(base_path('routes/web.php'));
         });
     }
