873739eafad4a905672c28721a411739a49cbf0e
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-10 23:02:51 +0630

    add prefix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,6 +34,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->prefix('api')
                 ->group(base_path('routes/web.php'));
         });
     }
