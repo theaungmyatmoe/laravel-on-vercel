5bcb9d2dce09e88ad38f9f8a6046ba60c65377a6
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-10 23:13:57 +0630

    change api name

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('appi')
+                ->prefix('api/v1')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
